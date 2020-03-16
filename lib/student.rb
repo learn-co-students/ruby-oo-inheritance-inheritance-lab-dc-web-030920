d51b3eec54d075f4f6f047ca1a2a5af093302872
kaidornel <kai_dornel@hotmail.com>
2020-03-16 13:38:56 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,29 @@
-class Student 
+class Student < User
+
+    attr_reader :first_name, :last_name
+    def initialize
+        @knowledge = []
+
+    end
+
+    def first_name
+        @first_name
+    end
+
+    def last_name
+        @last_name
+    end
+    
+    def new
+        @knowledge
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+    
+    def knowledge
+        @knowledge
+    end
 
 end
